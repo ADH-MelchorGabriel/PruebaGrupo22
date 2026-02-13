263c8ace3df5b1ca873c06fd77cc8742979e1e6c
Melchor Gabriel Lopez Castro <48580537+ADH-MelchorGabriel@users.noreply.github.com>
2026-02-13 10:42:25 -0700

    Inicial

---
+++ Prueba22.WEB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Prueba22.WEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
